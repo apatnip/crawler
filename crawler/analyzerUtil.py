2f2a5a58599ec6db86be54f5f576da3a9428c996
Aseem Patni <apatni1993@gmail.com>
2014-06-25 17:58:46 +0530

    fixed analyzerUtil.py Made things more clear.

--- crawler/analyzerUtil.py
+++ crawler/analyzerUtil.py
@@ -5,6 +5,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 from pprint import pprint
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+boxSize = 300
+moveSize = 300
+cordinates = sys.argv[1];
+image = sys.argv[2];
+img = cv2.imread(image)
+heightPage, widthPage  = img.shape[:2]
+
+# Checks if a link is inside a box
 def containsLink (x,y,link):
 	yLink = int(link['top'])
 	xLink = int(link['left'])
@@ -20,27 +29,41 @@ def containsLink (x,y,link):
 		return True
 	return False
 
+# Checks if a point is inside a box
 def containsPoint (x,y,xp,yp):
 	if xp>x and xp<x+boxSize:
 		if yp>y and yp<y+boxSize:
 			return True
 	return False
 
+# Draw grid and find no of links in each grid
+def getLinkDensity():
+	y=0
+	while True:	
+		x=left
+		if y+boxSize>heightPage:
+			break
+		while True:	
+			if x+boxSize>widthPage:
+				break
+			cv2.rectangle(img,(x,y),(x+boxSize,y+boxSize),(80,100,20),2)
+			counter=0
+			for link in data:
+				link = json.loads(link)	
+				if containsLink(x,y,link):
+					counter+=1
+		#	print counter
+			cv2.putText(img,str(counter),(x+boxSize/2,y+boxSize), font, 4,(0,0,0),2, cv2.FONT_HERSHEY_PLAIN)
+			x += moveSize
+		y += moveSize
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-boxSize = 300
-moveSize = 300
-cordinates = sys.argv[1];
-image = sys.argv[2];
-img = cv2.imread(image)
-heightPage, widthPage  = img.shape[:2]
-pprint(heightPage)
-
-left = widthPage
-right = 0
+# Read coordinates from file
 with open(cordinates) as data_file:    
     data = json.load(data_file)
 
+# Plot links in image and find margins
+left = widthPage
+right = 0
 for link in data:
 	link = json.loads(link)	
 	yLink = int(link['top'])
@@ -53,32 +76,18 @@ for link in data:
 		right = xLink+width 
 	cv2.rectangle(img,(xLink,yLink),(xLink+width,yLink+height),(40,50,200),2)
 
+# Draw margins
 cv2.line(img,(left,0),(left,heightPage),(255,0,0),5)
 cv2.line(img,(right,0),(right,heightPage),(255,0,0),5)
-ymax = (heightPage - heightPage%10) - boxSize
-print ymax
-y=0
-while True:	
-	x=0
-	if y+boxSize>heightPage:
-		break
-	while True:	
-		if x+boxSize>widthPage:
-			break
-		cv2.rectangle(img,(x,y),(x+boxSize,y+boxSize),(80,100,20),2)
-		counter=0
-		for link in data:
-			link = json.loads(link)	
-			if containsLink(x,y,link):
-				counter+=1
-		print counter
-		cv2.putText(img,str(counter),(x+boxSize/2,y+boxSize), font, 4,(0,0,0),2, cv2.FONT_HERSHEY_PLAIN)
-		x += moveSize
-	y += moveSize
 
+# Find number of links in boxes
+getLinkDensity()
 
+# Save image
 saveImage = image+'-analyzed.jpg'
 cv2.imwrite(saveImage,img)
+
+# Show image
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
